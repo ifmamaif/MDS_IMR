003e710ee6577b28cafdbe641b065d0a58cc649d
ifmamaif <ifmamaif@yahoo.com>
2018-04-22 22:30:56 +0300

    Version 2.00

---
+++ Project/Assets/Scripts/Item.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
+public class Item : ScriptableObject{
+	new public string name = "Name item";
+	public Sprite icon = null;
+	public bool isDefaultItem = false;
+
+	public virtual void Use(){
+		Debug.Log ("Using " + name);
+	}
+
+}
